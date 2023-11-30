1d945254ba739b76a62cde94d9945ba48a50444e
Mats100 <Muneeb75@icloud.com>
2023-12-01 02:38:15 +0500

    Files Added

---
+++ Assignment_4-213034/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment_4_213034
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    ///
+
+    public partial class MainWindow : Window
+    {
+        public class Person
+        {
+            private string _firstName;
+            private string _lastName;
+
+            public Person(string firstName, string lastName)
+            {
+                _firstName = firstName;
+                _lastName = lastName;
+            }
+            public string FirstName => _firstName;
+            public string LastName => _lastName;
+        }
+
+        public Person person { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            person = new Person("Muneeb", "Shahid");
+            DataContext = this;
+        }
+    }
+}
+
